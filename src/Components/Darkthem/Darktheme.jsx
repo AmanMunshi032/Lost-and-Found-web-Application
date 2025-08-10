fd38e88972ce066a1f2372838ce4c6f182b2a3d2
Tanvir islam <tanvirislam4147@gmail.com>
2025-08-10 17:22:11 +0600

    new page added

--- src/Components/Darkthem/Darktheme.jsx
+++ src/Components/Darkthem/Darktheme.jsx
@@ -1,26 +1,26 @@
-import { createContext, useContext, useEffect, useState } from "react";
-const ThemeContext = createContext('light');
+// import { createContext, useContext, useEffect, useState } from "react";
+// const ThemeContext = createContext('light');
 
- export const Darktheme = ({children}) => {
-     const [Darkmode, setDarkmode] = useState(
-        localStorage.getItem("theme")==="dark"
-     );
-     useEffect(()=>{
-        if(Darkmode){
-          document.documentElement.classList.add("dark")
-          localStorage.setItem("theme","dark")
-        }
-        else{
- document.documentElement.classList.remove("dark")
-          localStorage.setItem("theme","light")
-        }
-     },[Darkmode])
-    return (
-        <ThemeContext.Provider value={{Darkmode,setDarkmode}}>
-           {children}
-        </ThemeContext.Provider>
+//  export const Darktheme = ({children}) => {
+//      const [Darkmode, setDarkmode] = useState(
+//         localStorage.getItem("theme")==="dark"
+//      );
+//      useEffect(()=>{
+//         if(Darkmode){
+//           document.documentElement.classList.add("dark")
+//           localStorage.setItem("theme","dark")
+//         }
+//         else{
+//  document.documentElement.classList.remove("dark")
+//           localStorage.setItem("theme","light")
+//         }
+//      },[Darkmode])
+//     return (
+//         <ThemeContext.Provider value={{Darkmode,setDarkmode}}>
+//            {children}
+//         </ThemeContext.Provider>
         
-    );
-};
+//     );
+// };
 
- export const useDarkMode =()=>useContext(ThemeContext)
+//  export const useDarkMode =()=>useContext(ThemeContext)
